9ce404b5bcbbcb1ddfda628e16a8cab3f37f3c89
Zheng Lyu-Data <zheng.lyu1990@gmail.com>
2025-06-06 16:11:21 -0500

    Update data_science_salary.sql

--- 03_data_science_salary_analysis/data_science_salary.sql
+++ 03_data_science_salary_analysis/data_science_salary.sql
@@ -1,4 +1,4 @@
--- Create table for raw data -- Complete SQLite Table
+-- Create table for raw data -- Complete SQLite Script
 CREATE TABLE data_science_salaries_raw (
     work_year INTEGER,
     experience_level TEXT,
